d0ed6b4e56117728073c720ed8f9aec05455e942
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-10-06 22:57:28 +0100

    commit changes

--- resources/views/dashboard/deposit/deposit.blade.php
+++ resources/views/dashboard/deposit/deposit.blade.php
@@ -90,6 +90,7 @@
                         @csrf
                         <label for="">Select Payment Method</label>
                         <select name="payment_method_id" id="" class="form-control" required>
+                            <option disabled selected>Choose Payment Method</option>
                             @foreach($payment_m as $item)
                             <option value="{{ $item->id }}">{{ $item->name }}</option>
                             @endforeach
